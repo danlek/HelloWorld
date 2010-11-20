910a2c3af3a77a04e23d502560c363806af31586
danlek <gabriel.keldon@gmail.com>
2010-11-20 17:02:03 +0100

    Added super interactivity with a scanner zomg! Still testing addon...
    Signed-off-by: danlek <gabriel.keldon@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class HelloWorld 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("What is your name?");
+		String namn;
+		Scanner inkommande = new Scanner(System.in);
+		namn = inkommande.nextLine();
+		System.out.println("Hello " + namn);
 	}
 }
